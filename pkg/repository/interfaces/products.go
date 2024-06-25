bf39ea49ee0cc17964189c48c8918eef2d645a32
geothomas11 <geothms11@gmail.com>
2024-06-25 22:58:05 +0530

    inventory name changed as product

--- pkg/repository/interfaces/products.go
+++ pkg/repository/interfaces/products.go
@@ -7,7 +7,7 @@ import (
 
 type InventoryRepository interface {
 	AddInventory(inventory models.AddInventories) (models.InventoryResponse, error)
-	ListProducts(int,int) ([]models.InventoryUserResponse, error)
+	ListProducts(int, int) ([]models.InventoryUserResponse, error)
 	EditInventory(domain.Inventory, int) (domain.Inventory, error)
 	DeleteInventory(id string) error
 	CheckInventory(p_id int) (bool, error)
