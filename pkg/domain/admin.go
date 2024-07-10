29a2b88aeba07124d902af41a6d482e80e545a18
geothomas11 <geothms11@gmail.com>
2024-07-10 20:23:08 +0530

    updated

--- pkg/domain/admin.go
+++ pkg/domain/admin.go
@@ -2,12 +2,13 @@ package domain
 
 import "ShowTimes/pkg/utils/models"
 
-// type Admin struct {
-// 	ID       uint   `json:"id" gorm:"unique;not null"`
-// 	Name     string `json:"name" gorm:"validate:required"`
-// 	Username string `json:"email" gorm:"validate:required"`
-// 	Password string `json:"password" gorm:"validate:required"`
-// }
+type Admin struct {
+	ID       uint   `json:"id" gorm:"unique;not null"`
+	Name     string `json:"name" gorm:"validate:required"`
+	// Username string `json:"email" gorm:"validate:required"`
+	Email    string `json:"email" gorm:"validate:required"`
+	Password string `json:"password" gorm:"validate:required"`
+}
 
 type TokenAdmin struct {
 	Admin       models.AdminDetailsResponse
