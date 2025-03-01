3099e74053d7859765e6da031b96af8bc221bb67
Geo Thomas <geothms11@gmail.com>
2025-03-01 21:37:39 +0530

    testing files

--- pkg/api/handler/offer.go
+++ pkg/api/handler/offer.go
@@ -149,8 +149,6 @@ func (of *OfferHandler) GetCategoryOffer(c *gin.Context) {
 
 }
 
-//offer expires
-
 // ExpireProductOffer invalidates a product-specific offer.
 //
 // @Summary Expire Product Offer
