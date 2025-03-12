93568d3596ce7fe74eca7fa783700d2479d747c5
Scarface <geothms11@gmail.com>
2025-03-12 23:48:35 +0530

    updated  wallet also in earlier commit

--- pkg/utils/models/order.go
+++ pkg/utils/models/order.go
@@ -11,6 +11,7 @@ type OrderFromCart struct {
 	PaymentID uint `json:"payment_id" binding:"required"`
 	AddressID uint `json:"address_id" binding:"required"`
 	UseWallet bool `json:"use_wallet" gorm:"defalt:false"`
+	CouponID  uint `json:"coupon_id" `
 }
 type OrderSuccessResponse struct {
 	OrderID        uint    `json:"order_id"`
